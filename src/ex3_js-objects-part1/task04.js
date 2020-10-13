8324e9302909eebec3320e7c06e0082a33340163
Николай Смирнов <>
2020-10-13 14:05:39 +0300

    Замечания исправлены

--- src/ex3_js-objects-part1/task04.js
+++ src/ex3_js-objects-part1/task04.js
@@ -1,11 +1,12 @@
-function checkV(str, obj) {
+ function checkV(str, obj) {
     let newObj = obj;
 
-    if (str in newObj) {
+    if (!(str in newObj)) {
+        newObj[str] = 'new'
         return newObj;
     }
 
-    return (newObj[str] = 'new');
+    return newObj;
 }
 
 module.exports = checkV;
--- src/ex3_js-objects-part1/task06.js
+++ src/ex3_js-objects-part1/task06.js
@@ -3,7 +3,13 @@ function objCopy(obj) {
 
     for (let key in obj) {
         if (Array.isArray(obj[key])) {
-            objClone[key] = [...obj[key]];
+            obj[key].forEach(item => {
+                if (typeof item === 'object') {
+                    objClone[key] = [objCopy(item)];
+                } else {
+                    objClone[key] = obj[key];
+                }
+            })
         } else if (obj[key] !== Array.isArray(obj[key]) && typeof obj[key] === 'object') {
             objClone[key] = objCopy(obj[key]);
         } else {
